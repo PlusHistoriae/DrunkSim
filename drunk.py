96e2f5c5c7c85f1eb9e446ff6d2f1fe2344b3550
PlusHistoriae <85767310+PlusHistoriae@users.noreply.github.com>
2021-06-15 21:39:09 +0000

    Add Graph

--- drunk.py
+++ drunk.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import random
 
 DRINKER = 1 
@@ -5,9 +6,12 @@ MAXIMUM_TURNS = 10000000
 
 steps = 10
 
+data = []
+
 for turn in range(MAXIMUM_TURNS):
-  steps_back_and_forth = random.randint (-3, 3) 
+  steps_back_and_forth = random.randint (-4, 3) 
   steps = steps + steps_back_and_forth
+  data.append(steps)
   
   if steps > 20:
     print ("The Drinker has reached his house, and recovered.")
@@ -17,3 +21,6 @@ for turn in range(MAXIMUM_TURNS):
     print ("The Drinker has fallen off of a cliff")
     break
   print (steps)
+
+plt.plot(data)
+plt.show()
