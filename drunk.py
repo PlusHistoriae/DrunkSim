df7acfb2de4f34401e5b77afa2bda6ab5ec0ff42
PlusHistoriae <85767310+PlusHistoriae@users.noreply.github.com>
2021-06-15 21:34:07 +0000

    Simplification1

--- drunk.py
+++ drunk.py
@@ -6,9 +6,8 @@ MAXIMUM_TURNS = 10000000
 steps = 10
 
 for turn in range(MAXIMUM_TURNS):
-  step_forward = random.randint (1, 5)
-  step_backwards = random.randint (2, 4)
-  steps = steps + (step_forward - step_backwards)
+  steps_back_and_forth = random.randint (-3, 3) 
+  steps = steps + steps_back_and_forth
   
   if steps > 20:
     print ("The Drinker has reached his house, and recovered.")
